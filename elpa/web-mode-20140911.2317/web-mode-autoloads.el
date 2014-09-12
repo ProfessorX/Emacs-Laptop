adb48d210eaf4d4e0cbda5661b9c7bc41d924489
Abraham Xiao <abrahamx@debian>
2014-09-12 23:01:31 +0400

    Back to work. Nothing is not there to be negociated.

---
+++ elpa/web-mode-20140911.2317/web-mode-autoloads.el
@@ -0,0 +1,29 @@
+;;; web-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (web-mode) "web-mode" "web-mode.el" (21523 17044
+;;;;;;  424685 941000))
+;;; Generated autoloads from web-mode.el
+
+(autoload 'web-mode "web-mode" "\
+Major mode for editing web templates.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("web-mode-pkg.el" "wfs-mode.el") (21523
+;;;;;;  17044 450005 525000))
+
+;;;***
+
+(provide 'web-mode-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; web-mode-autoloads.el ends here
